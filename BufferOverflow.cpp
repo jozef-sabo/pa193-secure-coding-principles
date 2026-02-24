a609ac4f18d2733007d2b4f68c0eb93641af8de7
Jozef Sabo <31158086+jozef-sabo@users.noreply.github.com>
2026-02-24 10:46:55 +0100

    Fix security issues with strncpy

--- BufferOverflow.cpp
+++ BufferOverflow.cpp
@@ -83,7 +83,7 @@ void demoAdjacentMemoryOverflow(char* userName, char* password) {
 
 	memset(buf, 0, sizeof(buf));
 	memset(message, 1, sizeof(message));
-	strncpy(buf, userName, sizeof(buf));              // We will copy only characters which fits into buf
+	strncpy(buf, userName, sizeof(buf) - 1);              // We will copy only characters which fits into buf
 
 													  // Now print username to standard output - nothing sensitive, right?
 	sprintf(message, "Checking '%8s' password\n", buf);
